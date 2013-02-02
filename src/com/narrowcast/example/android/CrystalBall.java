ac327363b7f8ea9849a9dfc271fcbb08443765a5
Chee-Hyung Yoon <yoonchee@gmail.com>
2013-02-02 09:48:33 +0900

    Add CrystalBall to return answers.

---
+++ src/com/narrowcast/example/android/CrystalBall.java
@@ -0,0 +1,24 @@
+package com.narrowcast.example.android;
+
+import java.util.Random;
+
+public class CrystalBall {
+	
+	public String mAnswers[] = {
+			"It is certain",
+			"It is decidely so",
+			"All signs say YES",
+			"The stars are not aligned",
+			"My reply is no",
+			"It is doubtful",
+			"Better not tell you now",
+			"Concentrate and ask again",
+			"Unable to answer now" };
+
+	public String getAnAnswer() {
+		Random randomGenerator = new Random();
+		int randomNumber = randomGenerator.nextInt(mAnswers.length);
+		
+		return mAnswers[randomNumber];		
+	}
+}
--- src/com/narrowcast/example/android/MainActivity.java
+++ src/com/narrowcast/example/android/MainActivity.java
@@ -1,15 +1,35 @@
 package com.narrowcast.example.android;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	private CrystalBall mCrystalBall = new CrystalBall();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // Declare our View variables assign them the Views from the layout file
+        final TextView answerLabel = (TextView) findViewById(R.id.textView1);
+        Button getAnswerButton = (Button) findViewById(R.id.button1);
+        
+        getAnswerButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				String answer = mCrystalBall.getAnAnswer();
+				
+				// Update the label with our dynamic answer
+				answerLabel.setText(answer);
+			}
+		});
     }
 
     @Override
